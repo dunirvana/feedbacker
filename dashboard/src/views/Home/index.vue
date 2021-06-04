bba1a005b8c7f2df07398de1e73ce51ad99fcb02
Eduardo Teixeira <dunirvana@gmail.com>
2021-06-04 08:53:59 -0300

    Criação da tela inicial e inicio do modal factory

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
-  <contact />
-  <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">feedbacker - 2021</p>
+  <div>
+    <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+    <contact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+      <p class="font-medium text-center text-gray-800">feedbacker - 2021</p>
+    </div>
   </div>
 </template>
 
