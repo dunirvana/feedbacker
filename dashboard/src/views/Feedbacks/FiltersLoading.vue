61008d364b7ce3c00a27a1bf950b5a44a5dbc679
Eduardo Teixeira <dunirvana@gmail.com>
2021-07-14 08:52:29 -0300

    Criação da dashboard

---
+++ dashboard/src/views/Feedbacks/FiltersLoading.vue
@@ -0,0 +1,49 @@
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded"
+    width="100%"
+    height="300px"
+  >
+
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="50px"
+      animation-duration="2s"
+    >
+    </content-loader>
+
+    <content-loader
+      class="mt-3 rounded"
+      width="50%"
+      height="90px"
+      animation-duration="2.3s"
+    >
+    </content-loader>
+
+    <content-loader
+      class="mt-3 rounded"
+      width="50%"
+      height="90px"
+      animation-duration="2.7s"
+    >
+    </content-loader>
+
+    <content-loader
+      class="mt-3 rounded"
+      width="50%"
+      height="90px"
+      animation-duration="2.9s"
+    >
+    </content-loader>
+
+  </content-loader>
+</template>
+
+<script>
+import ContentLoader from '../../components/ContentLoader'
+
+export default {
+  components: { ContentLoader }
+}
+</script>
